ef6188d117c6b625ecf009788b92c1a490dc84e9
wyksean448 <wyksean448@gmail.com>
2025-02-09 00:20:01 +0800

    支持ws

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  env: {
+    TZ: 'Asia/Shanghai',
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.externals.push({
       'utf-8-validate': 'commonjs utf-8-validate',
       'bufferutil': 'commonjs bufferutil',
     })
     return config
-  },
-  env: {
-    TZ: 'Asia/Shanghai',
-  },
-  typescript: {
-    ignoreBuildErrors: true,
   },
 }
 
